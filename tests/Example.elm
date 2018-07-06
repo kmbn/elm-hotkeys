8853c95c73ba8cb9b076d81942c0d371b8ead309
kmbn <github@nevermindtheumlauts.com>
2018-07-06 16:59:39 +0200

    Set up Travis

---
+++ tests/Example.elm
@@ -0,0 +1,15 @@
+module Example exposing (..)
+
+import Expect
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    describe "Example test suite"
+        [ test "This is not that"
+            (\_ -> Expect.false
+                "Expected this to be different from that"
+                ("this" == "that")
+            )
+        ]
